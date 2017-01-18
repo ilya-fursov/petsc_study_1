9def487c67729d72e1e9a4e1d1ae7ddc82323d08
Ilya Fursov <ilya.foursov@mail.ru>
2017-01-18 10:54:37 +0000

    Added viewing of IS, AO, etc.

--- ex3.cpp
+++ ex3.cpp
@@ -23,7 +23,7 @@ int main(int argc,char **argv)
 
 	PetscErrorCode ierr;
 	int rank, size, N = 5;
-	double p1, p2, p3;
+	double p1, p2, p3, p4;
 	DM da;
 
 	// Set up the viewers
@@ -49,6 +49,7 @@ int main(int argc,char **argv)
 	ierr  = PetscOptionsGetReal(NULL,NULL,"-p1",&p1,NULL); CHKERRQ(ierr);
 	ierr  = PetscOptionsGetReal(NULL,NULL,"-p2",&p2,NULL); CHKERRQ(ierr);
 	ierr  = PetscOptionsGetReal(NULL,NULL,"-p3",&p3,NULL); CHKERRQ(ierr);
+	ierr  = PetscOptionsGetReal(NULL,NULL,"-p4",&p4,NULL); CHKERRQ(ierr);
 
 	ierr = PetscPrintf(PETSC_COMM_WORLD,"N = %d\n", N); CHKERRQ(ierr);
 
@@ -72,9 +73,12 @@ int main(int argc,char **argv)
 	ierr = DMDAVecGetArray(da, ga, &x); CHKERRQ(ierr);
 	
 	int i0, j0, m0, n0;
+	int i1, j1, m1, n1;
 	ierr = DMDAGetCorners(da, &i0, &j0, NULL, &m0, &n0, NULL); CHKERRQ(ierr);
+	ierr = DMDAGetGhostCorners(da, &i1, &j1, NULL, &m1, &n1, NULL); CHKERRQ(ierr);
 	//ierr = PetscPrintf(PETSC_COMM_WORLD, "i0 = %d, j0 = %d, m0 = %d, n0 = %d\n", i0, j0, m0, n0); CHKERRQ(ierr);
-	printf("RANK = %d\ti0 = %d, j0 = %d, m0 = %d, n0 = %d\n", rank, i0, j0, m0, n0);
+	printf("RANK = %d\tcorners i0 = %d, j0 = %d, m0 = %d, n0 = %d\n", rank, i0, j0, m0, n0);
+	printf("RANK = %d\tgh. cor i1 = %d, j1 = %d, m1 = %d, n1 = %d\n", rank, i1, j1, m1, n1);
 	
 	for (int i = i0; i < i0+m0; i++)
 		for (int j = j0; j < j0+n0; j++)
@@ -83,6 +87,12 @@ int main(int argc,char **argv)
 			x[j][i].b = i*N + j + 1000;
 			x[j][i].c = i*N + j + 2000;
 		}
+
+	//DMGlobalToLocalBegin(da, ga, INSERT_VALUES, la);
+	//DMGlobalToLocalEnd(da, ga, INSERT_VALUES, la);	
+	DMLocalToGlobalBegin(da, la, INSERT_VALUES, ga);
+	DMLocalToGlobalEnd(da, la, INSERT_VALUES, ga);		
+
 	ierr = PetscPrintf(PETSC_COMM_WORLD, "DEBUG-1\n"); CHKERRQ(ierr);	// DEBUG
 	ierr = DMDAVecRestoreArray(da, ga, &x); CHKERRQ(ierr);
 
@@ -96,12 +106,29 @@ int main(int argc,char **argv)
 	ierr = VecView(la, vascii); CHKERRQ(ierr);
 	ierr = PetscSleep(p3); CHKERRQ(ierr);
 
+	// Get mappings from DMDA
+	ISLocalToGlobalMapping map;
+	ierr = DMGetLocalToGlobalMapping(da, &map); CHKERRQ(ierr);
+	ierr = ISLocalToGlobalMappingView(map, view); CHKERRQ(ierr);
+	ierr = PetscViewerASCIIPrintf(vascii, "\nIS local to global:\n"); CHKERRQ(ierr);
+	ierr = ISLocalToGlobalMappingView(map, vascii); CHKERRQ(ierr);
+	ierr = PetscSleep(p4); CHKERRQ(ierr);
+	
+	AO ao;
+	ierr = DMDAGetAO(da, &ao); CHKERRQ(ierr);
+	ierr = AOView(ao, view); CHKERRQ(ierr);
+	ierr = PetscViewerASCIIPrintf(vascii, "\nAO:\n"); CHKERRQ(ierr);
+	ierr = AOView(ao, vascii); CHKERRQ(ierr);
+	ierr = PetscSleep(p4); CHKERRQ(ierr);
+
+
 	// Destroy objects
 	ierr = PetscViewerDestroy(&view); CHKERRQ(ierr);
 	ierr = PetscViewerDestroy(&vascii); CHKERRQ(ierr);
 	ierr = DMDestroy(&da); CHKERRQ(ierr);
 	ierr = VecDestroy(&ga); CHKERRQ(ierr);
-	ierr = VecDestroy(&la); CHKERRQ(ierr);
+	ierr = VecDestroy(&la); CHKERRQ(ierr); 
+	//ierr = ISLocalToGlobalMappingDestroy(map); CHKERRQ(ierr);	
 	ierr = PetscFinalize();
 
 	return 0;
